5a4f144be716dd2afbc7c53c78cc6376860d2cc2
Jorge Obiols <jorge.obiols@gmail.com>
2024-06-09 19:44:11 -0300

    [FIX] docker compose

---
+++ compose/copy_filestore.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Copiar el filestore a test y borrarlo de prod
+
+project="monaco"
+
+set -e
+
+if [ $# -eq 0 ]; then
+    echo "Error: Tenes que pasar el nombre de la base de datos de test."
+    echo "Uso: $0 <"$project"_test_xxxx>"
+    exit 1
+fi
+
+sudo cp -ar /odoo_ar/odoo-16.0/$project/data_dir/filestore/$1/ /odoo_st/odoo-16.0/$project/data_dir/filestore/
+sudo rm -r /odoo_ar/odoo-16.0/$project/data_dir/filestore/$1
